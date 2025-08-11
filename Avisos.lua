e9640aa85b98fb42bd3f93fad12e08290722aaed
NexorHub <hercules4328@gmail.com>
2025-08-10 21:26:10 -0300

    Update Avisos.lua

--- Avisos.lua
+++ Avisos.lua
@@ -1,8 +1,1 @@
-local TextChatService = game:GetService("TextChatService")
-local mensagem1 = "Salve rapaziada!! percebi que o script esta com um bug no speed boost"
-local mensagem2 = "Estou trabalhando para arrumar essa função, amanhã ja estará tudo arrumado!!"
-pcall(function()
-    TextChatService.TextChannels.RBXGeneral:SendAsync(mensagem1)
-    task.wait(1) --mensagem para algo importante?
-    TextChatService.TextChannels.RBXGeneral:SendAsync(mensagem2)
-end)
+
